0dbfe4fd29f45d791d8b44217aee5e5619d601a0
toanhtran <toanh_tran@yahoo.com>
2016-01-10 21:04:57 +0900

    first commit

---
+++ js/controllers/MainController.js
@@ -0,0 +1,53 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.today = new Date();
+
+  $scope.appetizers = [
+    {
+      name: 'Caprese',
+      description: 'Mozzarella, tomatoes, basil, balsmaic glaze.',
+      price: 4.95
+    },
+    {
+      name: 'Mozzarella Sticks',
+      description: 'Served with marinara sauce.',
+      price: 3.95
+    },
+    {
+      name: 'Bruschetta',
+      description: 'Grilled bread garlic, tomatoes, olive oil.',
+      price: 4.95
+    }
+  ];
+  $scope.mains = [
+    {
+      name: 'Veal',
+      description: 'Veal, tomatoes, basil, balsmaic glaze.',
+      price: 14.95
+    },
+    {
+      name: 'Eggplant Parm',
+      description: 'Breaded Eggplant, with parm cheese and sauce.',
+      price: 13.95
+    },
+    {
+      name: 'House Special Pizza',
+      description: 'Our house speciality hand toss pizza.',
+      price: 9.95
+    }
+  
+  ];
+  $scope.extras = [
+    {
+      name: 'Pistachio Gelato',
+      description: 'Yummy goodness',
+      price: 4.95
+    },
+    {
+      name: 'Tiramasu',
+      description: 'lady fingers soaked in espresso and covered is fluffly sweet cream sauce.',
+      price: 13.95
+    }
+  
+  ];
+
+}]);
